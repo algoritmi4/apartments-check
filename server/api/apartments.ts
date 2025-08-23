1d0c6ade0169ad1e46b3a30a2543b864cc7290e3
algoritmi4 <ivan.butorin@fintopio.com>
2025-08-23 19:09:02 +0300

    fix: api types and apartments endpoint logic

--- server/api/apartments.ts
+++ server/api/apartments.ts
@@ -1,19 +1,19 @@
 import type { Apartment } from '~/dataBase/db'
+import type { PaginatedResponse } from '~/types/api'
 import { apartments } from '~/dataBase/db'
 
-export interface ApartmentsResponse {
-  page: number
-  limit: number
-  total: number
-  items: Apartment[]
-}
+export type ApartmentsResponse = PaginatedResponse<Apartment>
 
 export interface ApartmentsQuery {
   page?: number
   limit?: number
+  priceSort?: 'abs' | 'desc'
   priceMin?: number
   priceMax?: number
   rooms?: number
+  floor?: number
+  floorSort?: 'abs' | 'desc'
+  squareSort?: 'abs' | 'desc'
   squareMin?: number
   squareMax?: number
 }
@@ -28,6 +28,7 @@ export default defineEventHandler(async (event) => {
   const offset = (page - 1) * limit
 
   let results = apartments
+  const total = results.length
 
   if (query.priceMin) {
     results = results.filter((item: Apartment) => item.price >= Number(query.priceMin))
@@ -39,6 +40,9 @@ export default defineEventHandler(async (event) => {
   if (query.rooms) {
     results = results.filter((item: Apartment) => item.rooms === Number(query.rooms))
   }
+  if (query.floor) {
+    results = results.filter((item: Apartment) => item.floors.current === Number(query.floor))
+  }
 
   if (query.squareMin) {
     results = results.filter((item: Apartment) => item.square >= Number(query.squareMin))
@@ -46,13 +50,43 @@ export default defineEventHandler(async (event) => {
   if (query.squareMax) {
     results = results.filter((item: Apartment) => item.square <= Number(query.squareMax))
   }
+  if (query.floorSort) {
+    results = results.sort((a: Apartment, b: Apartment) => {
+      if (query.floorSort === 'abs') {
+        return b.floors.current - a.floors.current
+      }
+      else {
+        return a.floors.current - b.floors.current
+      }
+    })
+  }
+  if (query.priceSort) {
+    results = results.sort((a: Apartment, b: Apartment) => {
+      if (query.priceSort === 'abs') {
+        return b.price - a.price
+      }
+      else {
+        return a.price - b.price
+      }
+    })
+  }
+  if (query.squareSort) {
+    results = results.sort((a: Apartment, b: Apartment) => {
+      if (query.floorSort === 'abs') {
+        return b.square - a.square
+      }
+      else {
+        return a.square - b.square
+      }
+    })
+  }
 
   const paginated = results.slice(offset, offset + limit)
 
   return {
     page,
     limit,
-    total: results.length,
+    total,
     items: paginated,
   }
 })
