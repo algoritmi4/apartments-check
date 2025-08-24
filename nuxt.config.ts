243e906fd5fcde6ef930a4a381b66fe0e519926f
algoritmi4 <ivan.butorin@fintopio.com>
2025-08-25 01:07:29 +0300

    feat: add base head tag for application

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,19 @@ import { defineNuxtConfig } from 'nuxt/config'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Apartments Check',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'canonical', href: 'https://apartments-check.netlify.app/' },
+      ],
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
