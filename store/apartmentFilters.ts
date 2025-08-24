0800cf006d70fdbf37a7c5ffeabde88e64b5edeb
algoritmi4 <ivan.butorin@fintopio.com>
2025-08-24 20:36:19 +0300

    fix: types in apartmentsFilter store and change sort mode to single

--- store/apartmentFilters.ts
+++ store/apartmentFilters.ts
@@ -2,26 +2,35 @@ import type { ApartmentsQuery } from '~/server/api/apartments'
 import type { SortParam } from '~/types/api'
 import { defineStore } from 'pinia'
 
-type SortableFields = 'price' | 'floor' | 'square'
-type RangedFields = 'price' | 'square'
-type SpecificFields = 'rooms'
+export type SortableFields = 'price' | 'floor' | 'square'
+export type RangedFields = 'price' | 'square'
+export type SpecificFields = 'rooms'
 
 export const useApartmentFiltersStore = defineStore('apartmentFilters', () => {
-  const filters: ApartmentsQuery = {}
+  const filters = reactive<ApartmentsQuery>({})
 
-  const setSortFilter = (field: SortableFields, value: SortParam) => {
+  const sortableFields = ['price', 'floor', 'square'] as const
+
+  const setSortFilter = (field: SortableFields, value: SortParam | undefined) => {
     filters[`${field}Sort`] = value
+
+    if (value) {
+      sortableFields.forEach((f) => {
+        if (f !== field)
+          filters[`${f}Sort`] = undefined
+      })
+    }
   }
 
-  const setMinFilter = (field: RangedFields, value: number) => {
+  const setMinFilter = (field: RangedFields, value: number | undefined) => {
     filters[`${field}Min`] = value
   }
 
-  const setMaxFilter = (field: RangedFields, value: number) => {
+  const setMaxFilter = (field: RangedFields, value: number | undefined) => {
     filters[`${field}Max`] = value
   }
 
-  const setSpecificFilter = (field: SpecificFields, value: number) => {
+  const setSpecificFilter = (field: SpecificFields, value: number | undefined) => {
     filters[field] = value
   }
 
